3f2505befa8e6466513b5fdab9721a43e5b2591e
unikforceit <freelancerakibul@gmail.com>
2023-07-14 21:27:28 +0600

    meta mask and wallet connect working

--- main.js
+++ main.js
@@ -1,90 +1,51 @@
 import {
-    EthereumClient,
-    w3mConnectors,
-    w3mProvider,
-    WagmiCoreChains,
-    WagmiCore
-  } from "https://unpkg.com/@web3modal/ethereum";
-  
-  import { Web3Modal } from "https://unpkg.com/@web3modal/html";
-  
-  //import { configureChains, createConfig, getAccount, readContract } from "https://unpkg.com/@wagmi/core";
-  
-  import { nft } from "./abi.js";
-  
-  // Equivalent to importing from @wagmi/core
-  const { configureChains, createConfig, getAccount, readContract } = WagmiCore
-  
-   // Equivalent to importing from @wagmi/core/chains
-   const { mainnet } = WagmiCoreChains
-  
-  const chains = [mainnet];
-  const projectId = "210b371a0406d0aa02a6c3a56fb9dda5";
-  
-  let account;
-  
-  const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
-  const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors: w3mConnectors({ projectId, chains }),
-    publicClient,
-  });
-  const ethereumClient = new EthereumClient(wagmiConfig, chains);
-  const web3modal = new Web3Modal({ projectId }, ethereumClient);
-  let accounts = await getAccount();
-      account = accounts?.address;
-      const toxic = await readContract({
-      address: "0x5ca8dd7f8E1Ee6D0c27a7BE6d9F33ef403fbcDD8",
-      abi: nft.abi,
-      functionName: "balanceOf",
-      args: [String(account)],
-    });
-    const som = await readContract({
-      address: "0x32e14d6f3dda2b95e505b14eb4552fd3eeaa1f0d",
-      abi: nft.abi,
-      functionName: "balanceOf",
-      args: [String(account)],
-    });
-//   document.getElementById("wallet-connect-button").addEventListener("click", async () => {
-//     await web3modal.openModal();
-//     // gets the account address
-//     let accounts = await getAccount();
-//     account = accounts?.address;
-//     console.log(account);
-//   });
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+  WagmiCore,
+  WagmiCoreChains,
+  WagmiCoreConnectors,
+} from "https://unpkg.com/@web3modal/ethereum@2.6.2";
 
-  document.getElementById('my-button').addEventListener('click', () => {
-    web3modal.openModal()
-  })
-//   document.getElementById("balance-button").addEventListener("click", async () => {
-//     // gets balance of user
-//     console.log(nft);
-//     const toxic = await readContract({
-//       address: "0x5ca8dd7f8E1Ee6D0c27a7BE6d9F33ef403fbcDD8",
-//       abi: nft.abi,
-//       functionName: "balanceOf",
-//       args: [String(account)],
-//     });
-//     const som = await readContract({
-//       address: "0x32e14d6f3dda2b95e505b14eb4552fd3eeaa1f0d",
-//       abi: nft.abi,
-//       functionName: "balanceOf",
-//       args: [String(account)],
-//     });
-  
-//     // console.info(isError, isLoading);
-//     document.getElementById("balance").innerText = Number(toxic);
-//     document.getElementById("som").innerText = Number(som);
-//     console.log(Number(toxic));
-//     console.log(Number(som));
-//   });
-//   $(document).ready(function() {
-//     function showOverlayText() {
-//       if (accounts?.isConnected) {
-//         $("#tsc-nft").text(toxic);
-//         $("#som-nft").text(som);
-//         $("#overlay").fadeIn().delay(1000).fadeOut();
-//       }
-//     }
-//     showOverlayText();
-//   });
+import { Web3Modal } from "https://unpkg.com/@web3modal/html@2.6.2";
+
+// 0. Import wagmi dependencies
+const { mainnet, polygon, avalanche, arbitrum } = WagmiCoreChains;
+const { configureChains, createConfig, getAccount, readContract } = WagmiCore;
+
+// 1. Define chains
+const chains = [mainnet, polygon, avalanche, arbitrum];
+const projectId = "210b371a0406d0aa02a6c3a56fb9dda5";
+let account;
+// 2. Configure wagmi client
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: [
+    ...w3mConnectors({ chains, version: 2, projectId }),
+    new WagmiCoreConnectors.CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "toxicskullsclub",
+      },
+    }),
+  ],
+  publicClient,
+});
+
+// 3. Create ethereum and modal clients
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
+export const web3Modal = new Web3Modal(
+  {
+    projectId,
+  },
+  ethereumClient
+);
+
+document.getElementById("wallet-connect-button").addEventListener("click", async () => {
+  await web3Modal.openModal();
+  // gets the account address
+  let accounts = await getAccount();
+  account = accounts?.address;
+  console.log(accounts);
+});
--- script.js
+++ script.js
@@ -2,87 +2,172 @@ import {
   EthereumClient,
   w3mConnectors,
   w3mProvider,
+  WagmiCore,
   WagmiCoreChains,
-  WagmiCore
-} from "https://unpkg.com/@web3modal/ethereum";
+  WagmiCoreConnectors,
+} from "https://unpkg.com/@web3modal/ethereum@2.6.2";
 
-import { Web3Modal } from "https://unpkg.com/@web3modal/html";
+import { Web3Modal } from "https://unpkg.com/@web3modal/html@2.6.2";
 
-//import { configureChains, createConfig, getAccount, readContract } from "https://unpkg.com/@wagmi/core";
+// 0. Import wagmi dependencies
+const { mainnet, polygon, avalanche, arbitrum } = WagmiCoreChains;
+const { configureChains, createConfig, getAccount, readContract } = WagmiCore;
 
-import { nft } from "./abi.js";
-
-// Equivalent to importing from @wagmi/core
-const { configureChains, createConfig, getAccount, readContract } = WagmiCore
-
- // Equivalent to importing from @wagmi/core/chains
- const { mainnet } = WagmiCoreChains
-
-const chains = [mainnet];
+// 1. Define chains
+const chains = [mainnet, polygon, avalanche, arbitrum];
 const projectId = "210b371a0406d0aa02a6c3a56fb9dda5";
-
+const cookieName = "walletConnected";
+const tscCookie = "tscCookie";
+const somCookie = "somCookie";
+const toxic = "0x5ca8dd7f8E1Ee6D0c27a7BE6d9F33ef403fbcDD8";
+const som = "0x32e14d6f3dda2b95e505b14eb4552fd3eeaa1f0d";
+const contractABI = [{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"previousAdmin","type":"address"},{"indexed":false,"internalType":"address","name":"newAdmin","type":"address"}],"name":"AdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"beacon","type":"address"}],"name":"BeaconUpgraded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint8","name":"version","type":"uint8"}],"name":"Initialized","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"implementation","type":"address"}],"name":"Upgraded","type":"event"},{"inputs":[],"name":"MAX_SUPPLY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address[]","name":"owners","type":"address[]"},{"internalType":"uint256[]","name":"tokenIds","type":"uint256[]"}],"name":"airdrop","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getImplementation","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"baseURI_","type":"string"}],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"proxiableUUID","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"salePrice","type":"uint256"}],"name":"royaltyInfo","outputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"baseURI_","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"},{"internalType":"uint96","name":"feeNumerator","type":"uint96"}],"name":"setDefaultRoyalty","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"tokensOfOwner","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"}],"name":"upgradeTo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"upgradeToAndCall","outputs":[],"stateMutability":"payable","type":"function"}];
 let account;
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiConfig = createConfig({
   autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
+  connectors: [
+    ...w3mConnectors({ chains, version: 2, projectId }),
+    new WagmiCoreConnectors.CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "toxicskullsclub",
+      },
+    }),
+  ],
   publicClient,
 });
+
+// 3. Create ethereum and modal clients
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
-const web3modal = new Web3Modal({ projectId }, ethereumClient);
-let accounts = await getAccount();
-    account = accounts?.address;
-    const toxic = await readContract({
-    address: "0x5ca8dd7f8E1Ee6D0c27a7BE6d9F33ef403fbcDD8",
-    abi: nft.abi,
-    functionName: "balanceOf",
-    args: [String(account)],
-  });
-  const som = await readContract({
-    address: "0x32e14d6f3dda2b95e505b14eb4552fd3eeaa1f0d",
-    abi: nft.abi,
-    functionName: "balanceOf",
-    args: [String(account)],
-  });
-// document.getElementById("my-button").addEventListener("click", async () => {
-//   await web3modal.openModal();
-//   // gets the account address
-//   let accounts = await getAccount();
-//   account = accounts?.address;
-//   document.getElementById("address").innerText = account;
-//   console.log(accounts);
-// });
+export const web3Modal = new Web3Modal(
+  {
+    projectId,
+  },
+  ethereumClient
+);
 
-// document.getElementById("balance-button").addEventListener("click", async () => {
-//   // gets balance of user
-//   console.log(nft);
-//   const toxic = await readContract({
-//     address: "0x5ca8dd7f8E1Ee6D0c27a7BE6d9F33ef403fbcDD8",
-//     abi: nft.abi,
-//     functionName: "balanceOf",
-//     args: [String(account)],
-//   });
-//   const som = await readContract({
-//     address: "0x32e14d6f3dda2b95e505b14eb4552fd3eeaa1f0d",
-//     abi: nft.abi,
-//     functionName: "balanceOf",
-//     args: [String(account)],
-//   });
-
-//   // console.info(isError, isLoading);
-//   document.getElementById("balance").innerText = Number(toxic);
-//   document.getElementById("som").innerText = Number(som);
-//   console.log(Number(toxic));
-//   console.log(Number(som));
-// });
 $(document).ready(function() {
-  function showOverlayText() {
-    if (accounts?.isConnected) {
-      $("#tsc-nft").text(toxic);
-      $("#som-nft").text(som);
-      $("#overlay").fadeIn().delay(1000).fadeOut();
+  $("#connectButton").on("click", connect);
+  $("#disconnectButton").on("click", disconnect);
+  $("#check-balance").on("click", checkBalance);
+
+  const isConnected = getCookie(cookieName) === "true";
+  if (isConnected) {
+    showConnectedUI();
+  }
+});
+
+async function connect() {
+    try {
+      await web3Modal.openModal();
+      setCookie(cookieName, true);
+      showConnectedUI();
+    } catch (error) {
+      console.log("Error:", error);
+    }
+}
+async function checkBalance(){
+  showNotice("Checking...");
+  // Use the web3 object for interacting with the blockchain
+  let accounts = await getAccount();
+  account = accounts?.address;
+  const tscCount = await readContract({
+    address: toxic,
+    abi: contractABI,
+    functionName: "balanceOf",
+    args: [String(account)],
+  });
+  const somCount = await readContract({
+    address: som,
+    abi: contractABI,
+    functionName: "balanceOf",
+    args: [String(account)],
+  });
+  updateNFTCount(tscCount, somCount);
+  showNotice(`You Have! TSC: ${tscCount}, and SOM: ${somCount}`);
+  setCookie(tscCookie, tscCount);
+  setCookie(somCookie, somCount);
+  setTimeout(hideNotice, 3000);
+  location.reload();
+}
+ function tscOffer() {
+  const tscData = getCookie(tscCookie);
+  const walletData = getCookie(cookieName);
+  if (walletData && tscData > 0) {
+    $("body").addClass("has-tsc");
+  } else {
+    $("body").removeClass("has-tsc");
+    $("body").addClass("no-nft");
+  }
+}
+
+function somOffer() {
+  const somData = getCookie(somCookie);
+  const walletData = getCookie(cookieName);
+
+  if (walletData && somData > 0) {
+    $("body").addClass("has-som");
+  } else {
+    $("body").removeClass("has-som");
+    $("body").addClass("no-nft");
+  }
+}
+
+  async function disconnect() {
+    await ethereumClient.disconnect();
+    console.log("Disconnected!");
+    showNotice("Disconnecting...");
+    setCookie(cookieName, false);
+    setCookie(tscCookie, 0);
+    setCookie(somCookie, 0);
+    location.reload();
+    showDisconnectedUI();
+    hideNotice();
+}
+
+function showConnectedUI() {
+  $("#connectButton").hide();
+  $("#disconnectButton").show();
+  $("#check-balance").show();
+  tscOffer();
+}
+
+function showDisconnectedUI() {
+  $("#connectButton").show();
+  $("#disconnectButton").hide();
+  $("#check-balance").hide();
+  tscOffer();
+}
+
+function showNotice(message) {
+  $("#notice").text(message);
+  $("#notice-overlay").fadeIn();
+}
+
+function hideNotice() {
+  $("#notice-overlay").fadeOut();
+}
+
+function updateNFTCount(count1, count2) {
+  $("#notice").html(`You Have! TSC: ${count1}<br>and SOM: ${count2}`);
+}
+
+function setCookie(name, value) {
+  document.cookie = `${name}=${value}; path=/`;
+}
+
+function getCookie(name) {
+  const cookieString = document.cookie;
+  const cookies = cookieString.split("; ");
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].split("=");
+    if (cookie[0] === name) {
+      return cookie[1];
     }
   }
-  showOverlayText();
-});
+
+  return "";
+}
